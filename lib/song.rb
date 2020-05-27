29e2959134611b584ff12ff2af34bfce9fe321de
Billy Ferrara <willfira717@gmail.com>
2020-05-27 18:01:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,10 @@ class Song
   end
 
   def self.new_from_filename(mp3_file)
-
-  end 
+    c = self.new
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    c
+  end
 
 end
